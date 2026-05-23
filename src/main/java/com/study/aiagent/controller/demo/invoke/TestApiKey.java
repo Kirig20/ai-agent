0868dffd09b360ee1d2e3ceb2dc769a254279a73
Kirig <511787652@qq.com>
2026-05-23 19:37:39 +0800

    4种不同方式调用AI大模型 + 使用 Ollama 部署1个本地大模型，并且通过SpringAl调用

---
+++ src/main/java/com/study/aiagent/controller/demo/invoke/TestApiKey.java
@@ -0,0 +1,12 @@
+package com.study.aiagent.controller.demo.invoke;
+
+/**
+ * 仅用于测试获取 API Key
+ */
+public interface TestApiKey {
+
+    // 修改为你的 API key
+//    String API_KEY = "你的 API Key";
+    String API_KEY = "you API Key";
+
+}
